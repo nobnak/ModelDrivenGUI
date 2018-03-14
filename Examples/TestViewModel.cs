ff71fa4c63a0d3d4cd2ccea71566063aca4c2a7e
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-03-15 01:19:05 +0900

    キャプチャ用モデル

--- Examples/TestViewModel.cs
+++ Examples/TestViewModel.cs
@@ -13,7 +13,7 @@ namespace ModelDrivenGUISystem.Examples {
         protected BaseView view;
 
         private void OnEnable() {
-            window = new Rect(10f, 10f, 200f, 300f);
+            window = new Rect(10f, 10f, 300f, 300f);
             var viewFactory = new SimpleViewFactory();
             view = ClassConfigurator.GenerateClassView(new BaseValue<object>(data), viewFactory);
         }
@@ -33,33 +33,24 @@ namespace ModelDrivenGUISystem.Examples {
 
         [System.Serializable]
         public class InnerInnerModel {
-            public bool bool01;
-            public int[] intArray01;
+            public bool i;
+            public int[] j;
         }
         [System.Serializable]
         public class InnerModel {
-            public int[] intArray01;
-            public InnerInnerModel innerInner;
+            public int[] g;
+            public InnerInnerModel h;
         }
         [System.Serializable]
         public class Model {
             public enum SimpleEnum { One, Two }
-#if false
-            public bool bool01;
-            public SimpleEnum enum01;
-            public string string01;
-            public int int01;
-            public float float01;
-            public Vector2 vec2_01;
-            public Vector3 vec3_01;
-            public Vector4 vec4_01;
-            public Vector2Int vec2int01;
+            public int a;
+            public SimpleEnum b;
+            public string c;
 
-            public InnerModel innerClass01;
-#endif
-            public int[] intArray01 = new int[0];
-            public InnerModel[] innerModelArray01 = new InnerModel[0];
-            public InnerInnerModel[] innerInnerArray01 = new InnerInnerModel[0];
+            public int[] d = new int[0];
+            public InnerModel[] e = new InnerModel[0];
+            public InnerInnerModel[] f = new InnerInnerModel[0];
         }
     }
 }
