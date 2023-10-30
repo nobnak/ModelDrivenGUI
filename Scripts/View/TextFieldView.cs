f7c0a59f8a8ddfcf1b58f9aad663fdf7a9688fb1
Nakata Nobuyuki <nobu.nakata@gmail.com>
2023-10-30 16:01:24 +0900

    フィールドのラベル幅の過不足にかかわらず、文字列をラップ禁止

--- Scripts/View/TextFieldView.cs
+++ Scripts/View/TextFieldView.cs
@@ -9,9 +9,18 @@ namespace ModelDrivenGUISystem.View {
     public class TextFieldView : BaseView {
         public ReactiveProperty<string> Input { get; set; }
 
-        public override void Draw() {
+		protected GUIStyle labelStyle;
+
+		public TextFieldView() {
+			labelStyle ??= new GUIStyle(UnityEngine.GUI.skin.label) {
+				alignment = TextAnchor.MiddleLeft,
+				wordWrap = false,
+			};
+		}
+
+		public override void Draw() {
             using (new GUILayout.HorizontalScope(new GUIContent(Title, Tooltip), GUIStyle.none)) {
-				GUILayout.Label(Title, GUILayout.ExpandWidth(false));
+				GUILayout.Label(Title, labelStyle, GUILayout.ExpandWidth(false));
                 Input.Value = GUILayout.TextField(Input.Value);
 
 			}
--- Scripts/View/VectorView.cs
+++ Scripts/View/VectorView.cs
@@ -9,9 +9,18 @@ namespace ModelDrivenGUISystem.View {
     public class VectorView : BaseView {
         public virtual ReactiveCollection<string> Input { get; set; }
 
-        public override void Draw() {
+		protected GUIStyle labelStyle;
+
+		public VectorView() {
+			labelStyle ??= new GUIStyle(UnityEngine.GUI.skin.label) {
+				alignment = TextAnchor.MiddleLeft,
+				wordWrap = false,
+			};
+		}
+
+		public override void Draw() {
             using (new GUILayout.HorizontalScope(new GUIContent(Title, Tooltip), GUIStyle.none)) {
-                GUILayout.Label(Title, GUILayout.ExpandWidth(false));
+                GUILayout.Label(Title, labelStyle, GUILayout.ExpandWidth(false));
 				for (var i = 0; i < Input.Count; i++) {
 					Input[i] = GUILayout.TextField(Input[i]);
 				}
